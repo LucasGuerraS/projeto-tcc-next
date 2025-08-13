00a046b25c203998025ee64219f0fb74b60315da
LucasGuerraS <lucasgasardo@gmail.com>
2025-08-12 22:22:48 -0300

    feat: create base of landing page visuals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import type { NextPage } from 'next';
+import styles from './index.module.css';
 
-export default function Home() {
+const LandingPage: NextPage = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className={styles.landingPage}>
+      <div className={styles.navigation}>
+        <div className={styles.items3}>
+          <div className={styles.home}>Home</div>
+          <div className={styles.button}>
+            <div className={styles.home}>Crie uma conta</div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={styles.designpatternsfactory}>
+          DesignPatternsFactory
+        </div>
+      </div>
+      <div className={styles.oQueVoc}>O que você vai ver aqui?</div>
+      <div className={styles.copyParent}>
+        <div className={styles.copy}>
+          <div className={styles.page}>Sistema de progressão</div>
+          <div className={styles.dentroDoSeu}>
+            Dentro do seu perfil é possível acompanhar sua progressão geral e
+            nas trilhas individuais.
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.copy1}>
+            <div className={styles.page}>Três trilhas principais</div>
+            <div className={styles.dentroDoSeu}>
+              Cada trilha corresponde à um design pattern de um tipo específico.
+            </div>
+          </div>
+        </div>
+        <div className={styles.copy2}>
+          <div className={styles.aulasInterativas}>Aulas interativas</div>
+          <div className={styles.nossasTrilhasSo}>
+            Nossas trilhas são divididas em módulos que ensinam em bloquinhos o
+            conceito por trás de cada módulo.
+          </div>
+        </div>
+      </div>
+      <div className={styles.qualOObjetivo}>Qual o objetivo desse projeto?</div>
+      <div className={styles.text}>
+        <div className={styles.text1}>
+          <div className={styles.ensinoDeFcil}>Ensino de fácil absroção</div>
+          <div className={styles.oContedo}>
+            O conteúdo é separado em tamanhos pequenos, para que possa ser
+            consumido na velocidade de cada um, independente da janela de tempo
+            disponível.
+          </div>
+        </div>
+        <div className={styles.text1}>
+          <div className={styles.page}>Acesso à informação</div>
+          <div className={styles.dentroDoSeu}>
+            Buscamos oferecer um conteúdo de qualidade para todos com uma sede
+            de conhecimento, por isso todo o acesso é totalmente gratuito!
+          </div>
+        </div>
+      </div>
+      <div className={styles.copy3}>
+        <div className={styles.pageTitle}>
+          <b className={styles.simplificandoPadresDe}>
+            Simplificando padrões de design para iniciantes e gurus
+          </b>
+          <div className={styles.aprendaEAplique}>
+            Aprenda e aplique o conhecimento de design patterns de maneira
+            simples e didática, atravé de uma experiência imersiva e intuitiva!
+          </div>
+        </div>
+        <div className={styles.button1}>
+          <div className={styles.home}>Crie uma conta</div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default LandingPage;
