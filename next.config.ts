a1aa923cd4b83d9702402d0a96fa22019aab50fb
LucasGuerraS <lucasgasardo@gmail.com>
2025-08-22 17:10:21 -0300

    feature: finish implementing part 1 of all modules

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
       new URL(
         'https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/oficinadosbits/media/uploads/produtos/foto/zurojqnv/file.png'
       ),
+      new URL(
+        'https://pocketstop.com/wp-content/uploads/2021/07/Factory%20against%20blue%20cloudy%20sky-1.jpeg'
+      ),
+      new URL(
+        'https://dreammaker.co.uk/wp-content/uploads/2022/02/feeling-alone.jpg'
+      ),
     ],
   },
 };
