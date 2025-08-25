d3c209a38c226c3abaa6e106f53d944d089bd016
LucasGuerraS <lucasgasardo@gmail.com>
2025-08-25 17:00:13 -0300

    feature: define some constants in code editor component

--- src/components/internal/codeEditor/codeEditor.tsx
+++ src/components/internal/codeEditor/codeEditor.tsx
@@ -1,21 +1,25 @@
-'use client'
+'use client';
 
 import { Editor } from '@monaco-editor/react';
 
 type CodeEditorProps = {
-    content: string;
-    height: string | undefined;
-    width: string | undefined;
+  content: string;
+  height?: string;
+  width?: string;
 };
+
+const LANGUAGE = 'java';
+
+const THEME = 'vs-dark';
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ content, height, width }) => {
   return (
     <Editor
-      height={height}
-      width={width}
-      defaultLanguage='java'
+      height={height ? height : '20vh'}
+      width={width ? width : '30%'}
+      defaultLanguage={LANGUAGE}
       defaultValue={content}
-      theme='vs-dark'
+      theme={THEME}
     />
   );
 };
