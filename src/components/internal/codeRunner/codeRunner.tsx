df498ae5e2decd7d8c35bcd70014e4aed8c56054
LucasGuerraS <lucasgasardo@gmail.com>
2025-08-26 20:35:29 -0300

    feat: use base component on code runner instead of code editor

--- src/components/internal/codeRunner/codeRunner.tsx
+++ src/components/internal/codeRunner/codeRunner.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import CodeEditor from '../codeEditor/codeEditor';
 import CodeConsole from '../codeConsole/codeConsole';
+import { Editor } from '@monaco-editor/react';
 
 type CodeRunnerProps = {
   content: string;
@@ -9,15 +9,31 @@ type CodeRunnerProps = {
   editorHeight: number;
 };
 
+const LANGUAGE = 'java';
+
+const THEME = 'vs-dark';
+
 const CodeRunner: React.FC<CodeRunnerProps> = ({ content, width, editorHeight }) => {
   return (
     <div className={`w-${width}/100 h-auto flex flex-col mb-4 bg-stone-500`}>
-      <CodeEditor
-        content={content}
+      <Editor
+        defaultValue={content}
         height={`${editorHeight}vh`}
         width='100%'
-        minimap={false}
-        readOnly={true}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+          padding: {
+            top: 8,
+            bottom: 0,
+          },
+          lineNumbers: 'on',
+          wordWrap: 'on',
+          overviewRulerBorder: false,
+        }}
+        defaultLanguage={LANGUAGE}
+        theme={THEME}
       />
       <CodeConsole />
     </div>
