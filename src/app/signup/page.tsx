e67195cb3226906483becedd7faad59d4399d226
LucasGuerraS <lucasgasardo@gmail.com>
2025-09-03 17:07:56 -0300

    feature: optimize signup page to be able to handle form logic

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,45 +1,117 @@
-'use client'
+'use client';
 
 import type { NextPage } from 'next';
 import styles from './index.module.css';
+import { Input } from '@/components/ui/input';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  email: z.email(),
+  password: z.string().min(8).max(100),
+});
 
 const Signup: NextPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
     <div className={styles.signup}>
       <div className={styles.navigation}>
         <div className={styles.items}>
-          <div className={styles.home} onClick={() => window.location.href = '/'}>Home</div>
+          <div
+            className={styles.home}
+            onClick={() => (window.location.href = '/')}
+          >
+            Home
+          </div>
         </div>
         <div className={styles.designpatternsfactory}>
           DesignPatternsFactory
         </div>
       </div>
-      <div className={styles.form}>
-        <div className={styles.heading}>
-          <div className={styles.crieSuaConta}>Crie sua conta!</div>
-        </div>
-        <div className={styles.input}>
-          <div className={styles.nomeDeUsurio}>Nome de usuário</div>
-          <div className={styles.field}>
-            <div className={styles.label}>Smitherton</div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className={styles.form}
+        >
+          <div className={styles.heading}>
+            <div className={styles.crieSuaConta}>Crie sua conta!</div>
           </div>
-        </div>
-        <div className={styles.input1}>
-          <div className={styles.nomeDeUsurio}>Enderço de email</div>
-          <div className={styles.field}>
-            <div className={styles.label}>email@janesfakedomain.net</div>
-          </div>
-        </div>
-        <div className={styles.input2}>
-          <div className={styles.nomeDeUsurio}>Senha</div>
-          <div className={styles.field2}>
-            <div className={styles.label}>12345678@a</div>
-          </div>
-        </div>
-        <div className={styles.button} onClick={() => window.location.href = '/profile'}>
-          <div className={styles.criar}>Criar</div>
-        </div>
-      </div>
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormItem className={styles.input2}>
+                <FormLabel className={styles.nomeDeUsurio}>
+                  Nome de usuário
+                </FormLabel>
+                <FormControl className={styles.field2}>
+                  <Input
+                    type='text'
+                    {...field}
+                    placeholder='Digite seu nome de usuario'
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem className={styles.input2}>
+                <FormLabel className={styles.nomeDeUsurio}>Email</FormLabel>
+                <FormControl className={styles.field2}>
+                  <Input
+                    type='email'
+                    {...field}
+                    placeholder='Digite seu email'
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem className={styles.input2}>
+                <FormLabel className={styles.nomeDeUsurio}>Senha</FormLabel>
+                <FormControl className={styles.field2}>
+                  <Input
+                    type='password'
+                    {...field}
+                    placeholder='Digite sua senha'
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className={styles.button}>
+            Criar
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
